f59812d357a64aea2bfd2326debe90d779eff970
Shahnewaz Leon <leon.it.ju@gmail.com>
2022-09-12 16:00:19 -0400

    Adds an index view and set up appropriate urls

--- smarttodo/settings.py
+++ smarttodo/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'smarttodo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'todo.sqlite3',
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC−04:00'
 
 USE_I18N = True
 
