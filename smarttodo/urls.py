783db86801d51046037d88882ea0c91f3977c10b
dr-doc-phd <74687769+dr-doc-phd@users.noreply.github.com>
2022-09-18 16:09:33 -0400

    added login page w/ the helpful guidence of Janet and Arjun

--- smarttodo/urls.py
+++ smarttodo/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('todo/', include('todo.urls')),
+    path('', include('todo.urls')),
     path('admin/', admin.site.urls),
 ]
--- todo/views.py
+++ todo/views.py
@@ -17,3 +17,6 @@ def listitems(request, list_id):
     except List.DoesNotExist:
         raise Http404("List does not exist")
     return render(request, 'todo/list_items.html', {'list': todo_list})
+
+def login(request):
+    return render(request, 'todo/login.html')
