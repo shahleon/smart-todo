5e2dc405b2cc3a8b972dab82ea3a03aa9d732d48
Dong Li <729790146@qq.com>
2022-10-01 17:49:18 -0400

    add foreign key of List(every List belongs to a user)

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 
+class User(models.Model):
+    user_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return "%s" % self.user_name
+
 class List(models.Model):
     title_text = models.CharField(max_length=100)
     created_on = models.DateTimeField()
     updated_on = models.DateTimeField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     objects = models.Manager()
 
@@ -22,16 +32,6 @@ class ListItem(models.Model):
 
     def __str__(self):
         return "%s: %s" % (self.item_text, self.is_done)
-
-
-class User(models.Model):
-    user_name = models.CharField(max_length=100)
-    password = models.CharField(max_length=50)
-
-    objects = models.Manager()
-
-    def __str__(self):
-        return "%s" % self.user_name
 
 
 class UserTodoTemplate(models.Model):
