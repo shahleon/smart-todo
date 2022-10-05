420d582b04a07fc40fc16a2286db8986cba46758
kuochengyun <kisstvxqyunjae@gmail.com>
2022-10-04 20:46:16 -0400

    Add files via upload

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.http import Http404, HttpResponse
 from todo.models import List
 from todo.models import Template
 from todo.forms import NewUserForm
+from django.conf import settings
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
@@ -14,6 +15,7 @@ from django.db.models.query_utils import Q
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -76,16 +78,16 @@ def logout_request(request):
 	return redirect("todo:index")
 
 def password_reset_request(request):
-	if request.method == "POST":
-		password_reset_form = PasswordResetForm(request.POST)
-		if password_reset_form.is_valid():
-			data = password_reset_form.cleaned_data['email']
-			associated_users = User.objects.filter(Q(email=data))
-			if associated_users.exists():
-				for user in associated_users:
-					subject = "Password Reset Requested"
-					email_template_name = "todo/password/password_reset_email.txt"
-					c = {
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            associated_users = User.objects.filter(Q(email=data))
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "todo/password/password_reset_email.txt"
+                    c = {
 					"email":user.email,
 					'domain':'127.0.0.1:8000',
 					'site_name': 'Website',
@@ -93,12 +95,17 @@ def password_reset_request(request):
 					"user": user,
 					'token': default_token_generator.make_token(user),
 					'protocol': 'http',
-					}
-					email = render_to_string(email_template_name, c)
-					try:
-						send_mail(subject, email, 'ckuo3@ncsu.edu' , [user.email], fail_silently=False)
-					except BadHeaderError:
-						return HttpResponse('Invalid header found.')
-					return redirect ("/password_reset/done/")
-	password_reset_form = PasswordResetForm()
-	return render(request=request, template_name="todo/password/password_reset.html", context={"password_reset_form":password_reset_form})
+                    }
+                    email = render_to_string(email_template_name, c)
+                    try:
+                        send_email = EmailMessage(subject, email, settings.EMAIL_HOST_USER, [user.email])
+                        send_email.fail_silently = False
+                        send_email.send()
+						#send_mail(subject, email, 'ckuo3@ncsu.edu' , [user.email], fail_silently=False)
+                    except BadHeaderError:
+                    
+                        return HttpResponse('Invalid header found')
+                        
+                    return redirect("/password_reset/done/")
+    password_reset_form = PasswordResetForm()
+    return render(request=request, template_name="todo/password/password_reset.html", context={"password_reset_form":password_reset_form})
