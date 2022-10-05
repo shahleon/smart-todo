b80db5fb0dad9d1b20c587f85f9116ea99ac49f9
Dong Li <729790146@qq.com>
2022-10-05 16:25:14 -0400

    add saving todo list test case and yml file for travis-ci automation

--- todo/tests/test_views.py
+++ todo/tests/test_views.py
@@ -1,8 +1,16 @@
-from django.test import TestCase, Client
 from django.urls import reverse
+from django.utils import timezone
+from django.test import TestCase, Client
 
 from todo.models import List
 import json
+
+
+class TodoListTestCase(TestCase):
+    def testSavingTodoList(self):
+        response = self.client.get(reverse('createNewTodoList'))
+        self.assertEqual(response.status_code, 200)
+        print(response)
 
 
 class TestViews(TestCase):
