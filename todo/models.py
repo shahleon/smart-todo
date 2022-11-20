c5bcf2f0218415b14f0f062b44e5048b408672ee
jackson910210 <32348727+jackson910210@users.noreply.github.com>
2022-11-20 17:22:03 -0500

    Add start time and time it take to finish
    Modify models.py, views.py index.html, list_items.html

--- todo/models.py
+++ todo/models.py
@@ -31,6 +31,7 @@ class ListItem(models.Model):
     item_text = models.CharField(max_length=100)
     is_done = models.BooleanField(default=False)
     created_on = models.DateTimeField()
+    finished_on = models.DateTimeField()
     list = models.ForeignKey(List, on_delete=models.CASCADE)
 
     objects = models.Manager()
@@ -55,7 +56,7 @@ class TemplateItem(models.Model):
     item_text = models.CharField(max_length=100)
     created_on = models.DateTimeField()
     template = models.ForeignKey(Template, on_delete=models.CASCADE)
-
+    finished_on = models.DateTimeField()
     objects = models.Manager()
 
     def __str__(self):
